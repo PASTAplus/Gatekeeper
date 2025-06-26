5d049c9db5e8f5c063634e618d08520f486b67bc
Mark Servilla <mark.servilla@gmail.com>
2025-06-26 23:11:09 +0000

    Consolidate FastAPI APIrouter decorators for both package and audit filters (#12)

--- webapp/filter/package.py
+++ webapp/filter/package.py
@@ -32,143 +32,11 @@ client = httpx.AsyncClient(base_url=Config.PACKAGE, timeout=Config.TIMEOUT)
 
 
 @router.get("/package/{path:path}")
-async def package_get(request: Request, path: str):
-    try:
-        pasta_token = await authenticate(request=request)
-    except (AuthenticationException, ExpiredTokenException, InvalidTokenException) as ex:
-        status = ex.args[1]
-        msg = ex.args[0]
-        logger.error(f"{status}: {msg}")
-        return fastapi.responses.PlainTextResponse(f"{status}: {msg}", status_code=status)
-    req_headers = await make_request_headers(pasta_token, request)
-    params = str(request.query_params)
-    body = await request.body()
-    content = body.decode("utf-8")
-    req = client.build_request(
-        method="GET",
-        url=clean_path(path),
-        headers=req_headers,
-        params=params,
-        content=content
-    )
-    try:
-        response = await client.send(req, stream=True)
-        resp_headers = make_response_headers(pasta_token, response)
-        return StreamingResponse(
-            content=response.aiter_raw(),
-            background=BackgroundTask(response.aclose),
-            headers=resp_headers,
-            status_code=response.status_code
-        )
-    except httpx.HTTPError as ex:
-        logger.error(ex)
-        raise ex
-
-
 @router.post("/package/{path:path}")
-async def package_post(request: Request, path: str):
-    try:
-        pasta_token = await authenticate(request=request)
-    except (AuthenticationException, ExpiredTokenException, InvalidTokenException) as ex:
-        status = ex.args[1]
-        msg = ex.args[0]
-        logger.error(f"{status}: {msg}")
-        return fastapi.responses.PlainTextResponse(f"{status}: {msg}", status_code=status)
-    req_headers = await make_request_headers(pasta_token, request)
-    params = str(request.query_params)
-    body = await request.body()
-    content = body.decode("utf-8")
-    req = client.build_request(
-        method="POST",
-        url=clean_path(path),
-        headers=req_headers,
-        params=params,
-        content=content
-    )
-    try:
-        response = await client.send(req, stream=True)
-        resp_headers = make_response_headers(pasta_token, response)
-        return StreamingResponse(
-            content=response.aiter_raw(),
-            background=BackgroundTask(response.aclose),
-            headers=resp_headers,
-            status_code=response.status_code
-        )
-    except httpx.HTTPError as ex:
-        logger.error(ex)
-        raise ex
-
-
 @router.put("/package/{path:path}")
-async def package_put(request: Request, path: str):
-    try:
-        pasta_token = await authenticate(request=request)
-    except (AuthenticationException, ExpiredTokenException, InvalidTokenException) as ex:
-        status = ex.args[1]
-        msg = ex.args[0]
-        logger.error(f"{status}: {msg}")
-        return fastapi.responses.PlainTextResponse(f"{status}: {msg}", status_code=status)
-    req_headers = await make_request_headers(pasta_token, request)
-    params = str(request.query_params)
-    body = await request.body()
-    content = body.decode("utf-8")
-    req = client.build_request(
-        method="PUT",
-        url=clean_path(path),
-        headers=req_headers,
-        params=params,
-        content=content
-    )
-    try:
-        response = await client.send(req, stream=True)
-        resp_headers = make_response_headers(pasta_token, response)
-        return StreamingResponse(
-            content=response.aiter_raw(),
-            background=BackgroundTask(response.aclose),
-            headers=resp_headers,
-            status_code=response.status_code
-        )
-    except httpx.HTTPError as ex:
-        logger.error(ex)
-        raise ex
-
-
 @router.delete("/package/{path:path}")
-async def package_delete(request: Request, path: str):
-    try:
-        pasta_token = await authenticate(request=request)
-    except (AuthenticationException, ExpiredTokenException, InvalidTokenException) as ex:
-        status = ex.args[1]
-        msg = ex.args[0]
-        logger.error(f"{status}: {msg}")
-        return fastapi.responses.PlainTextResponse(f"{status}: {msg}", status_code=status)
-    req_headers = await make_request_headers(pasta_token, request)
-    params = str(request.query_params)
-    body = await request.body()
-    content = body.decode("utf-8")
-    req = client.build_request(
-        method="DELETE",
-        url=clean_path(path),
-        headers=req_headers,
-        params=params,
-        content=content
-    )
-    try:
-        response = await client.send(req, stream=True)
-        resp_headers = make_response_headers(pasta_token, response)
-        return StreamingResponse(
-            content=response.aiter_raw(),
-            background=BackgroundTask(response.aclose),
-            headers=resp_headers,
-            status_code=response.status_code
-        )
-    except httpx.HTTPError as ex:
-        logger.error(ex)
-        raise ex
-
-
 @router.head("/package/{path:path}")
-async def package_head(request: Request, path: str):
+async def package_filter(request: Request, path: str):
     try:
         pasta_token = await authenticate(request=request)
     except (AuthenticationException, ExpiredTokenException, InvalidTokenException) as ex:
@@ -181,7 +49,7 @@ async def package_head(request: Request, path: str):
     body = await request.body()
     content = body.decode("utf-8")
     req = client.build_request(
-        method="HEAD",
+        method=request.method,
         url=clean_path(path),
         headers=req_headers,
         params=params,
